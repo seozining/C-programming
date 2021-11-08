231eeaef0b7f386153f17ab7e2fad7cbfd85901f
Seojin <90236049+seozining@users.noreply.github.com>
2021-11-08 23:38:38 +0900

    Add files via upload

---
+++ hw5.c
@@ -0,0 +1,13 @@
+﻿'Project23.exe'(Win32): 'C:\Users\윤서진\source\repos\Project23\Debug\Project23.exe'을(를) 로드했습니다. 기호가 로드되었습니다.
+'Project23.exe'(Win32): 'C:\Windows\SysWOW64\ntdll.dll'을(를) 로드했습니다. 
+'Project23.exe'(Win32): 'C:\Windows\SysWOW64\kernel32.dll'을(를) 로드했습니다. 
+'Project23.exe'(Win32): 'C:\Windows\SysWOW64\KernelBase.dll'을(를) 로드했습니다. 
+'Project23.exe'(Win32): 'C:\Windows\SysWOW64\vcruntime140d.dll'을(를) 로드했습니다. 
+'Project23.exe'(Win32): 'C:\Windows\SysWOW64\ucrtbased.dll'을(를) 로드했습니다. 
+0x1aa0 스레드가 종료되었습니다(코드: 0 (0x0)).
+'Project23.exe'(Win32): 'C:\Windows\SysWOW64\kernel.appcore.dll'을(를) 로드했습니다. 
+'Project23.exe'(Win32): 'C:\Windows\SysWOW64\msvcrt.dll'을(를) 로드했습니다. 
+'Project23.exe'(Win32): 'C:\Windows\SysWOW64\rpcrt4.dll'을(를) 로드했습니다. 
+0x5314 스레드가 종료되었습니다(코드: 0 (0x0)).
+0x3ec8 스레드가 종료되었습니다(코드: 0 (0x0)).
+'[15708] Project23.exe' 프로그램이 종료되었습니다(코드: 0 (0x0)).
